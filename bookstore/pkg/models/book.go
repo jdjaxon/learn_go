d30cd7ea127fcece0f597768e5cd8309798cba5f
Jeremy Jackson <jeremy.d.jackson3@gmail.com>
2022-04-15 16:05:20 -0400

    Added functionality to create, delete, and get books from the DB in books.go.

--- bookstore/pkg/models/book.go
+++ bookstore/pkg/models/book.go
@@ -19,3 +19,27 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(ID int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", ID).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
