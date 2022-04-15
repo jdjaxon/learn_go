2b921252dc2c78b2e754ebd454c99113e21f1432
Jeremy Jackson <jeremy.d.jackson3@gmail.com>
2022-04-15 16:53:01 -0400

    Fixed function name for GetBookByID in bookstore-routes.go.

--- bookstore/pkg/routes/bookstore-routes.go
+++ bookstore/pkg/routes/bookstore-routes.go
@@ -8,7 +8,7 @@ import (
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookByID).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
