eac5d013bf9a5a130456ddb6b183a1e0ce50b2dc
Jeremy Jackson <jeremy.d.jackson3@gmail.com>
2022-04-15 14:37:02 -0400

    Added logic for deleteMovie.

--- crud_api/main.go
+++ crud_api/main.go
@@ -107,4 +107,14 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 } // updateMovie
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+		}
+	}
+
+	json.NewEncoder(w).Encode(movies)
 } // deleteMovie
