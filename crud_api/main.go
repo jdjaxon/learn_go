67ccdffc84f330e57e824c5d9513d9b8c3409bb2
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:54:37 -0400

    Added logic for createMovie.

--- crud_api/main.go
+++ crud_api/main.go
@@ -82,6 +82,12 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 } // getMovie
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
 } // createMovie
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
