ea551492bea5c3268e400f783cd1056aa053c958
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:54:37 -0400

    Adding completed example file from chapter 1.

--- blackhat_go/chapter1/hello.go
+++ blackhat_go/chapter1/hello.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"time"
+)
 
 type Person struct {
 	Name string
@@ -23,6 +29,48 @@ type Dog struct{}
 
 func (d *Dog) SayHello() {
 	fmt.Println("Woof woof")
+}
+
+func checkType(i interface{}) {
+	switch i.(type) {
+	case int:
+		fmt.Println("INTEGER!!!")
+	case string:
+		fmt.Println("STRING!!!")
+	default:
+		fmt.Println("UNKNOWN!!!")
+	}
+}
+
+// For concurrency example
+func f() {
+	fmt.Println("f function concurrency example")
+}
+
+func strlen(s string, c chan int) {
+	c <- len(s)
+}
+
+// Error handling example
+type MyError string
+
+func (e MyError) Error() string {
+	return string(e)
+}
+
+func foo() error {
+	return errors.New("Some error Occurred")
+}
+
+// For Handling Structured Data example
+type FooJSON struct {
+	Bar string
+	Baz string
+}
+
+type FooXML struct {
+	Bar string `xml:"id,attr"`
+	Baz string `xml:"parent>child"`
 }
 
 // Testing and demonstrating the concepts above as a refresher.
@@ -65,5 +113,52 @@ func main() {
 	default:
 		fmt.Println("Default case")
 	}
+
+	fmt.Print("count is an ")
+	checkType(count)
+	fmt.Print("str is an ")
+	checkType(str)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	var nums = []int{2, 4, 6, 8, 10}
+	for idx, val := range nums {
+		fmt.Println(idx, val)
+	}
 	///// Control Structures /////
+
+	///// Concurrency /////
+	go f()
+	// Needed so that main doesn't exit before f() is done
+	time.Sleep(1 * time.Second)
+	fmt.Println("main function")
+
+	// Go provides a type called channels which allow goroutines to sync and
+	// communicate.
+	var c = make(chan int)
+	go strlen("Salutations", c)
+	go strlen("World", c)
+	x, y := <-c, <-c
+	fmt.Println(x, y, x+y)
+	///// Concurrency /////
+
+	///// Error Handling /////
+	if err := foo(); err != nil {
+		fmt.Println("ERROR!!!")
+	}
+	///// Error Handling /////
+
+	///// Handling Structured Data /////
+	jsonData := FooJSON{"Joe Junior", "Hello Shabado"}
+	b1, _ := json.Marshal(jsonData)
+	fmt.Println(string(b1))
+	json.Unmarshal(b1, &jsonData)
+
+	xmlData := FooXML{"Joe Junior", "Hello Shabado"}
+	b2, _ := xml.Marshal(xmlData)
+	fmt.Println(string(b2))
+	json.Unmarshal(b2, &xmlData)
+	///// Handling Structured Data /////
 }
