1acb5a55864d5ad1503272a4269cc8969b393775
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:54:37 -0400

    Added logic for updateMovie.

--- crud_api/main.go
+++ crud_api/main.go
@@ -91,6 +91,19 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 } // createMovie
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 } // updateMovie
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
