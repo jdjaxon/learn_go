f3170d9888fa971557c99cca9cf60549eb15c07f
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:54:37 -0400

    Built the helloHandler.

--- webserver/main.go
+++ webserver/main.go
@@ -21,6 +21,17 @@ func main() {
 } // main
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
 } // helloHandler
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
