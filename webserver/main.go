fb68abf27e8ac8c21e2bfb63a5915fa93a1fde9a
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:54:37 -0400

    Built the server's formHandler.

--- webserver/main.go
+++ webserver/main.go
@@ -35,4 +35,15 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 } // helloHandler
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v\n", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 } // formHandler
